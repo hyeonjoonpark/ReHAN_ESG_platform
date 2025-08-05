8d9588d281c184d279895f430fa3446bb36ace8e
hyeonjoonpark <pjjoon1379@gmail.com>
2025-08-05 21:05:03 +0900

    serial ?

--- frontend/src/components/StartSplitBandSections.tsx
+++ frontend/src/components/StartSplitBandSections.tsx
@@ -5,8 +5,8 @@ const StartSplitBandSections: React.FC = () => {
     <section className="lg:col-span-2 flex flex-col justify-start space-y-8">
       <section className="space-y-8 bg-gray-800 rounded-2xl p-6 border border-white/20 h-full flex items-center justify-center">
         <div className="flex flex-col justify-center text-center items-center">
-          <h2 className="text-6xl font-extrabold mb-6">띠 분리하기</h2>
-          <p className="text-3xl leading-snug">
+          <h2 className="text-6xl font-extrabold mb-6 text-white dark:text-white">띠 분리하기</h2>
+          <p className="text-3xl leading-snug text-white dark:text-white">
             페트병의 입구 부분을 <br />
             띠 분리기에 넣어주세요.
           </p>
